4c469248c05dbce146447a6794a808c02f68316e
Tushar Chander <tusharchander18@gmail.com>
2023-10-12 18:42:05 +0530

    Hashing and Salting

--- Secrets+-+Starting+Code/Secrets - Starting Code/app.js
+++ Secrets+-+Starting+Code/Secrets - Starting Code/app.js
@@ -1,6 +1,6 @@
 //jshint esversion:6
 require('dotenv').config();
-
+const bcrypt = require("bcrypt");
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
@@ -12,7 +12,7 @@ app.use(express.static("public"));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 
-
+const saltRounds = 10;
 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 const userSchema =new mongoose.Schema( {
@@ -24,9 +24,7 @@ const userSchema =new mongoose.Schema( {
 
 const User = mongoose.model("User",userSchema);
 
-User.find().then((user)=>{
-    console.log(user);
-})
+
 
 app.get('/',(req,res)=>{
     res.render('home');
@@ -38,17 +36,20 @@ app.get('/login',(req,res)=>{
 
 app.post("/login",(req,res)=>{
     const email = req.body.username;
-    const pass = md5(req .body.password);
+    const pass = (req .body.password);
 
-    User.findOne({email:email,password:pass}).then((user)=>{
-        if(user){
-            console.log("user found!");
-            res.redirect("/secrets");
-        }
-        else{
-            console.log("No user found!");
-            res.redirect('/login');
-        }
+    User.findOne({email:email}).then((user)=>{
+        bcrypt.compare(pass, user.password, function(err, result) {
+            // result == true
+            if(result == true){
+                console.log("user found!");
+                res.redirect("/secrets");
+            }
+            else{
+                console.log("No user found!");
+                res.redirect('/login');
+            }
+        });
     });
     
 });
@@ -59,18 +60,21 @@ app.get('/register',(req,res)=>{
 
 app.post('/register',(req,res)=>{
     const email = req.body.username;
-    const pass = md5(req .body.password);
-    if(email!="" && pass!=""){
+    const pass = (req .body.password);
+    bcrypt.hash(pass, saltRounds , function(err, hash) {
+        
         const user = new User({
             email:email,
-            password:pass
+            password:hash
         });
         user.save().then((user)=>{
             console.log("User succesfully added",user);
             
+            res.redirect("/");
         });
-    }
-    res.redirect("/");
+    });
+   
+    
 });
 
 
